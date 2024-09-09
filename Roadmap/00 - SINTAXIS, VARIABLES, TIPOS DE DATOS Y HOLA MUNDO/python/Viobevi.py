69b4ca7e1cc7f328bb565317ffdb05636b7fa38a
Viobevi <purpleflowervbv@gmail.com>
2024-09-09 10:52:29 +0200

    Create Viobevi.py
    mi primera prueba con el video

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Viobevi.py
@@ -0,0 +1,33 @@
+#https://python.org
+
+#Comentario en una linea
+
+"""
+Mi primer comentario
+programando
+Novata 4life
+"""
+
+'''
+Mi primer comentario
+programando
+'''
+
+
+my_variable = "Mi variabe"
+my_variable = "Segunda variable aprendiendo"
+
+MY_CONSTANT = " Mi constante"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = 'Mi datos primitivos'
+
+print("Hola, Phyton!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(mybool))
+print(typw(my_string))
